0d9c0cdd17a068b1a70f3626e220cd241735a7f9
gopivenkatasaiteja1 <40701802+gopivenkatasaiteja1@users.noreply.github.com>
2023-12-02 17:08:54 -0600

    Update stop_container.sh
    Adding stop container.sh scripot to the deployment spec

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,3 +3,9 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi" 
+
+# Stopping all the running containers , -q displays only container id's
+docker container stop $(docker ps -q)
+
+#Removing all containers after they are stopped , using -a flog so it displays containers in all states
+docker container rm $(docker ps -a -q)
